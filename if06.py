914d600d052500450eded5f50150c4f0711ce28f
Your Name <ubaydullayev@samtuit.uz>
2022-01-02 22:57:00 +0500

    6-misol

--- if06.py
+++ if06.py
@@ -1,4 +1,32 @@
 def main(n):
+    digit1 = n % 10
+    n //= 10
+    answer = 1
+    maxi = digit1
+    
+    digit2 = n % 10
+    n //= 10
+    if digit2 > maxi:
+        answer = 2
+        maxi = digit2
+    
+    digit3 = n % 10
+    n //= 10
+    if digit3 > maxi:
+        answer = 3
+        maxi = digit3
+    
+    digit4 = n % 10
+    n //= 10
+    if digit4 > maxi:
+        answer = 4
+        maxi = digit4
+    
+    digit5 = n % 10
+    if digit5 > maxi:
+        answer = 5
+        
+        
     """
     Find the index of the largest digit of the five-digit number.
     Args:
@@ -6,4 +34,4 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    return answer
