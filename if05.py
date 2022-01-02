80de3c1d74d276e31b772b321bbf792c026f0522
Your Name <ubaydullayev@samtuit.uz>
2022-01-02 22:44:20 +0500

    5-misol

--- if05.py
+++ if05.py
@@ -1,4 +1,26 @@
 def main(n):
+    digit1 = n % 10
+    n //= 10
+    answer = digit1
+    
+    digit2 = n % 10
+    n //= 10
+    if digit2 > answer:
+        answer = digit2
+    
+    digit3 = n % 10
+    n //= 10
+    if digit3 > answer:
+        answer = digit3
+    
+    digit4 = n % 10
+    n //= 10
+    if digit4 > answer:
+        answer = digit4
+    
+    digit5 = n % 10
+    if digit5 > answer:
+        answer = digit5
     """
     Find the largest digit of the five-digit number.
     Args:
@@ -6,4 +28,4 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    return answer
