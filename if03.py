74806bb4816c526ae12c82192f3a57bb2702d279
Your Name <ubaydullayev@samtuit.uz>
2022-01-02 22:31:08 +0500

    3-misol

--- if03.py
+++ if03.py
@@ -1,4 +1,20 @@
 def main(a,b,c):
+    answer = 0
+    if a >= b:
+        if a <= c:
+            answer = a
+        elif b >= c:
+            answer = b
+        else:
+            answer = c
+    elif b >= a:
+        if b <= c:
+            answer = b
+        elif a >= c:
+            answer = a
+        else:
+            answer = c
+     
     """
     Determine the number between large and small.
     Args:
@@ -8,4 +24,4 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    return answer
